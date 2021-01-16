3ea8c0f5ada9bd2e45560df9c0d59681f5956b73
Awais Shafique <awais.shafique71@gmail.com>
2021-01-16 19:21:37 +0500

    Task_1_Completed

---
+++ Assets/Scripts/MoveObject/MoveObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+
+    bool isInit = true;
+
+    float speed;
+
+    public Transform target;
+
+    bool doneMoving;
+
+   public void Init(float speed , Transform target)
+   {
+        
+        this.speed = speed;
+        this.target = target;
+        isInit = true;
+    }
+
+
+    private void Update()
+    {
+        if(isInit)
+        {
+            if (doneMoving)
+                return;
+
+            Vector3 movePlayer = (target.transform.position - transform.position);
+
+            movePlayer.Normalize();
+
+            transform.position += movePlayer * speed * Time.deltaTime ;
+
+            if(Vector3.Distance(transform.position  , target.position) < 0.5f)
+            {
+                transform.position = target.transform.position;
+                doneMoving = true;
+                GetComponent<Collider>().enabled = false;
+            }
+
+        }
+    }
+
+
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var obj = other.GetComponent<MoveObject>();
+        if (obj)
+        {
+            if(obj.GetSpeed() < speed)
+            {
+                Destroy(obj.gameObject);
+            }
+
+            else
+            {
+                Destroy(this.gameObject);
+            }
+            
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Task/TaskController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskController : MonoBehaviour
+{
+
+
+    public GameObject pointA, pointB;
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            SpawnObject(pointA, pointB);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnObject(pointB, pointA);
+        }
+
+
+    }
+
+
+    void SpawnObject(GameObject point , GameObject target)
+    {
+        var g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        var r = g.AddComponent<Rigidbody>();
+
+        r.useGravity = false;
+
+        g.GetComponent<Collider>().isTrigger = true;
+        g.transform.position = point.transform.position;
+
+        var cube = g;
+
+        var moveObject = cube.AddComponent<MoveObject>();
+
+        var speed = Random.Range(1f, 5f);
+
+        moveObject.Init(speed, target.transform);
+
+    }
+
+
+}
